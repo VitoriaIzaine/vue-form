265e0c5fc0d831776410f2a8f242deac900bb9c3
vitoriaIzaine <vitoriaizaine@gmail.com>
2021-11-09 07:39:37 -0300

    hello

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <Router-view />
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+:root {
+  --red: #c22a1f;
 }
 
-#nav {
-  padding: 30px;
+@import url("https://fonts.googleapis.com/css2?family=Varela+Round&display=swap");
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+* {
+  font-family: "Varela Round", sans-serif;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html {
+  font-size: 62.5%;
 }
 </style>
